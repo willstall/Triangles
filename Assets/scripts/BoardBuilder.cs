b6ad833b3aaf19d2f72ef328c878d5ba4f497725
greg <banovg@gmail.com>
2016-12-30 01:05:17 -0500

    board builder

---
+++ Assets/scripts/BoardBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardBuilder : MonoBehaviour {
+	public GameObject boxObject;
+	public float radius = 10.0f;
+	public int quality = 25;
+	// Use this for initialization
+	void Start () {
+			float circumference = radius*2f*Mathf.PI;
+			float scale = circumference / (float)quality;
+			float adjustedRadius = radius+(scale/2f);
+		for(int i=0;i<quality;i+=1){
+			GameObject box = Instantiate(boxObject,
+					new Vector3(
+						Mathf.Cos(i/(float)quality*2f*Mathf.PI)*adjustedRadius,
+						Mathf.Sin(i/(float)quality*2f*Mathf.PI)*adjustedRadius,
+						0f),
+					//new Vector3(0f,0f,i/(float)quality*360f)
+					Quaternion.identity
+					);
+			box.transform.Rotate(new Vector3(0f,0f,(float)i/(float)quality*360f));
+			box.transform.localScale = new Vector3(scale,scale,scale);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
