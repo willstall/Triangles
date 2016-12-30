ccc4461a9d7d04f1d9232f7c507690d6d34eb8f9
greg <banovg@gmail.com>
2016-12-30 02:32:18 -0500

    starting rotation and forward motion

---
+++ Assets/scripts/StartingRandomRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingRandomRotation : MonoBehaviour {
+	public bool randomX = true;
+	public bool randomY = true;
+	public bool randomZ = true;
+	// Use this for initialization
+	void Start () {
+		this.transform.Rotate(
+			randomX?Random.value*360f:0f,
+			randomY?Random.value*360f:0f,
+			randomZ?Random.value*360f:0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
