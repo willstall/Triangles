90e24fba1ed159b81335b02cc73c70a0ec853bf3
greg <banovg@gmail.com>
2016-12-30 04:03:47 -0500

    puck... spherecast isnt working :(

---
+++ Assets/scripts/Puck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puck : MonoBehaviour {
+	public int goalNumber = 20;
+	int number = 0;
+	//RaycastHit hit;
+	int layerMask = 1 << 6;
+	float radius;
+	// Use this for initialization
+	void Start () {
+		radius = this.GetComponent<SphereCollider>().radius;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit[] hits = Physics.SphereCastAll(Vector3.zero,radius,Vector3.zero,0f);
+		number = hits.Length;
+
+		if (number>=goalNumber){Debug.Log("you have "+number+" triangles!!!");}
+	}
+}
